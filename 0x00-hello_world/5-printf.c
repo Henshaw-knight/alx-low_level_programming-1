b369882156d3ecb51a26b3d87eaadb97b5215c8e
Henshaw <josephhensh2000@gmail.com>
2022-09-09 18:43:39 -0700

    Create file and add prints the expected sentence

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Program that prints the expected word in the exact way'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
