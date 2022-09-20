c9e1c4dddc3cf276a5bc9e734da1c21b4108a3c2
Henshaw <josephhensh2000@gmail.com>
2022-09-19 19:02:34 -0700

    Update file after formatting with betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'C program to print the size of various types'
+ *
+ * Return: Always 0 (Success)
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
