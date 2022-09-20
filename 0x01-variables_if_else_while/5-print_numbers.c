c6fdde40e904bb7669aaab6758e412a4e004d0b7
Henshaw <josephhensh2000@gmail.com>
2022-09-19 21:22:01 -0700

    Add program that print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ **
+ * Description: 'Program that prints the expected word in the exact way'
+ **
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
