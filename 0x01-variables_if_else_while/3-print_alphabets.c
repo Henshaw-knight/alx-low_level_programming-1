2e06ddd6b4b815b7bddb328e778ee066e528106f
Henshaw <josephhensh2000@gmail.com>
2022-09-19 20:26:49 -0700

    Create program that prints the alphabet in lowercase and uppercase using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Program that prints the expected word in the exact way'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+	}
+	putchar(\n);
+
+	return (0);
+}
