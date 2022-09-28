979beaaf6ea2b64098e5d3f05ecfd3d1cbc2225d
Henshaw <josephhensh2000@gmail.com>
2022-09-28 07:42:57 -0700

    Update file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,14 +14,10 @@ void print_square(int size)
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
-			{
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
 	else
-	{
-		_putchar('\n');
-	}
+		_putchar('\n')
 }
