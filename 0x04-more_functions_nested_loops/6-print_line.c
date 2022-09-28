09b9cbec4b1f227f68cb444f85db1aafd9f1f017
Henshaw <josephhensh2000@gmail.com>
2022-09-28 04:47:32 -0700

    Add function that draws a straight line in the terminal based on the number of n

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: the number of _ to be printed to form the line
+ *
+ * Return: Nothing
+ */
+void print_line(int n)
+{
+	int i = 0;
+	
+	while (i <= n)
+	{
+		if (i > 0)
+			_putchar('_');
+		_putchar('\n')
+	}
+}
