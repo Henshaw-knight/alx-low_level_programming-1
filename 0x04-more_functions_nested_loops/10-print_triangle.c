e1526db5fa5a97a88ed9bb5d305e41aad7fa3839
Henshaw <josephhensh2000@gmail.com>
2022-09-29 03:40:13 -0700

    Add function that prints a triangle based on the size paramenter, followed by a new line

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints a triangle
+ * @size: the size of the triangle
+ * Return: Nothing
+ */
+void print_triangle(int size)
+{
+	int i, space, hash;
+
+	if size <= 0
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; size++)
+		{
+			for (space = i; space < size; space++)
+			{
+				_putchar(' ');
+			}
+
+			for (hash = 0; hash < i; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
