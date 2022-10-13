fa7197c2e6ec9cb46a01cbdd82eece3e465988d2
Henshaw <josephhensh2000@gmail.com>
2022-10-12 19:44:29 -0700

    Update file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -24,8 +24,10 @@ void print_array(int *a, int n)
 			printf(" ");
 		}
 		printf("%d", a[index]);
-		printf("\n");
 	}
-	else
-		printf("%d", *a);
+	else if (n == 0 || n < 0)
+	{
+		printf("%d", a[0]);
+	}
+	prinf("\n");
 }
